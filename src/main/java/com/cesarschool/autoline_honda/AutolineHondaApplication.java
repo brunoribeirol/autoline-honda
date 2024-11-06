0cf8c1368e707670ccc7ed882d48b8f516e461ac
Bruno Ribeiro <araujoribeiro.bruno@gmail.com>
2024-11-06 12:20:50 -0300

    "Initial commit"

---
+++ src/main/java/com/cesarschool/autoline_honda/AutolineHondaApplication.java
@@ -0,0 +1,13 @@
+package com.cesarschool.autoline_honda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutolineHondaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutolineHondaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cesarschool/autoline_honda/AutolineHondaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesarschool.autoline_honda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutolineHondaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
